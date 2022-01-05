93b12b591fdbd843c794b9ca52bd7802713fc0fe
Delino <64547069+d3lino@users.noreply.github.com>
2022-01-05 14:37:27 +0100

    Current date displayed: done

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,29 @@
 const currentDate = new Date();
 
 function currentDayToString(currentDate) {
-	const currentDay = currentDate.getDay();
 	const frDays = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
-
-	return frDays[currentDay];
+	return frDays[currentDate.getDay()];
 }
 
-dayText.innerHTML = currentDayToString(currentDate);
+function currentMonthToString(currentDate) {
+	const frMonths = [
+		'Janvier',
+		'Février',
+		'Mars',
+		'Avril',
+		'Mai',
+		'Juin',
+		'Juillet',
+		'Août',
+		'Septembre',
+		'Octobre',
+		'Novembre',
+		'Décembre'
+	]
+	return frMonths[currentDate.getMonth()];
+}
+
+document.getElementById('dayNb').innerText   = currentDate.getUTCDate();
+document.getElementById('month').innerText   = currentMonthToString(currentDate);
+document.getElementById('year').innerText    = currentDate.getUTCFullYear();
+document.getElementById('dayText').innerText = currentDayToString(currentDate);
